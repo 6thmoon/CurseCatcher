e9c72fc2ec9f741b90dad1018066b8a50a1c7310
6thmoon <59669640+6thmoon@users.noreply.github.com>
2024-08-30 16:25:17 -0700

    Improve formatting.

--- Artifact.cs
+++ Artifact.cs
@@ -9,165 +9,164 @@ using UnityEngine;
 using UnityEngine.Networking;
 using Resources = CurseCatcher.Properties.Resources;
 
-namespace Local.Eclipse.CurseCatcher
+namespace Local.Eclipse.CurseCatcher;
+
+static class Artifact
 {
-	static class Artifact
+	static ArtifactDef definition = null;
+	static int ruleIndex = -1;
+
+	[HarmonyPatch(typeof(ArtifactCatalog), nameof(ArtifactCatalog.SetArtifactDefs))]
+	[HarmonyPostfix]
+	static void CreateArtifact()
 	{
-		static ArtifactDef definition = null;
-		static int ruleIndex = -1;
+		definition = new ArtifactDef {
+				nameToken = "Artifact of Infliction",
+				descriptionToken = "Only enemies inflict permanent damage.",
 
-		[HarmonyPatch(typeof(ArtifactCatalog), nameof(ArtifactCatalog.SetArtifactDefs))]
-		[HarmonyPostfix]
-		static void CreateArtifact()
+				smallIconSelectedSprite = Load(Resources.enabled),
+				smallIconDeselectedSprite = Load(Resources.disabled),
+
+				artifactIndex = (ArtifactIndex) ArtifactCatalog.artifactDefs.Length,
+				cachedName = "Curse"
+			};
+
+		ArrayUtils.ArrayAppend(ref ArtifactCatalog.artifactDefs, definition);
+		Harmony instance = null;
+
+		RunArtifactManager.onArtifactEnabledGlobal +=
+			( RunArtifactManager _, ArtifactDef artifact ) =>
+			{
+				if ( instance is null && NetworkServer.active && artifact == definition )
+					instance = Harmony.CreateAndPatchAll(typeof(Plugin));
+			};
+
+		RunArtifactManager.onArtifactDisabledGlobal +=
+			( RunArtifactManager _, ArtifactDef artifact ) =>
+			{
+				if ( artifact == definition )
+				{
+					instance?.UnpatchSelf();
+					instance = null;
+				}
+			};
+	}
+
+	static Sprite Load(byte[] imageData)
+	{
+		Texture2D texture = new(512, 512, TextureFormat.ARGB32, 4, linear: false);
+		ImageConversion.LoadImage(texture, imageData);
+
+		return Sprite.Create(
+				texture, new Rect(0, 0, texture.width, texture.height),
+				pivot: new Vector2(texture.width / 2, texture.height / 2)
+			);
+	}
+
+	[HarmonyPatch(typeof(RuleCatalog), nameof(RuleCatalog.Init))]
+	[HarmonyFinalizer]
+	static void AddArtifact(Exception __exception)
+	{
+		if ( __exception is not null ) throw __exception;
+		RuleDef rule = RuleDef.FromArtifact(definition.artifactIndex);
+
+		ruleIndex = RuleCatalog.allRuleDefs.Count;
+		rule.globalIndex = ruleIndex;
+
+		RuleCatalog.allRuleDefs.Add(rule);
+		RuleCatalog.ruleDefsByGlobalName[rule.globalName] = rule;
+
+		rule.category = RuleCatalog.artifactRuleCategory;
+		RuleCatalog.artifactRuleCategory.children.Add(rule);
+
+		for ( int i = 0; i < rule.choices.Count; ++i )
 		{
-			definition = new ArtifactDef {
-					nameToken = "Artifact of Infliction",
-					descriptionToken = "Only enemies inflict permanent damage.",
+			RuleChoiceDef choice = rule.choices[i];
 
-					smallIconSelectedSprite = Load(Resources.enabled),
-					smallIconDeselectedSprite = Load(Resources.disabled),
+			choice.localIndex = i;
+			choice.globalIndex = RuleCatalog.allChoicesDefs.Count;
 
-					artifactIndex = (ArtifactIndex) ArtifactCatalog.artifactDefs.Length,
-					cachedName = "Curse"
-				};
+			RuleCatalog.allChoicesDefs.Add(choice);
+			RuleCatalog.ruleChoiceDefsByGlobalName[choice.globalName] = choice;
+		}
+	}
 
-			ArrayUtils.ArrayAppend(ref ArtifactCatalog.artifactDefs, definition);
-			Harmony instance = null;
+	[HarmonyPatch(typeof(RuleCatalog), nameof(RuleCatalog.AddRule))]
+	[HarmonyPrefix]
+	static bool SkipRule(RuleDef ruleDef)
+	{
+		foreach ( RuleChoiceDef choice in ruleDef.choices )
+			if ( choice.artifactIndex == definition.artifactIndex )
+				return false;
 
-			RunArtifactManager.onArtifactEnabledGlobal +=
-				( RunArtifactManager _, ArtifactDef artifact ) =>
-				{
-					if ( instance is null && NetworkServer.active && artifact == definition )
-						instance = Harmony.CreateAndPatchAll(typeof(Plugin));
-				};
+		return true;
+	}
 
-			RunArtifactManager.onArtifactDisabledGlobal +=
-				( RunArtifactManager _, ArtifactDef artifact ) =>
-				{
-					if ( artifact == definition )
+	[HarmonyPatch(typeof(EclipseRun), nameof(EclipseRun.OverrideRuleChoices))]
+	[HarmonyPostfix]
+	static void ShowInEclipse(
+			RuleChoiceMask mustInclude, RuleChoiceMask mustExclude)
+	{
+		IEnumerable<RuleChoiceDef> choices = ruleIndex >= 0 ?
+				RuleCatalog.GetRuleDef(ruleIndex).choices : new List<RuleChoiceDef>();
+
+		foreach ( RuleChoiceDef choice in choices )
+		{
+			mustInclude[choice.globalIndex] = false;
+			mustExclude[choice.globalIndex] = false;
+		}
+	}
+
+	[HarmonyPatch(typeof(NetworkExtensions), nameof(NetworkExtensions.Write),
+			new Type[] { typeof(NetworkWriter), typeof(RuleBook) })]
+	[HarmonyPrefix]
+	static bool WriteRuleBook(NetworkWriter writer, RuleBook src)
+	{
+		for ( int i = 0; i < src.ruleValues.Length; ++i )
+			if ( i != ruleIndex )
+				writer.Write(src.ruleValues[i]);
+
+		return false;
+	}
+
+	[HarmonyPatch(typeof(NetworkExtensions), nameof(NetworkExtensions.ReadRuleBook))]
+	[HarmonyPrefix]
+	static bool ReadRuleBook(NetworkReader reader, RuleBook dest)
+	{
+		for ( int i = 0; i < dest.ruleValues.Length; ++i )
+		{
+			if ( i == ruleIndex )
+			{
+				dest.ruleValues[i] =
+						(byte) RuleCatalog.GetRuleDef(ruleIndex).defaultChoiceIndex;
+			}
+			else dest.ruleValues[i] = reader.ReadByte();
+		}
+
+		return false;
+	}
+
+	[HarmonyPatch(typeof(RunArtifactManager), nameof(RunArtifactManager.OnDeserialize))]
+	[HarmonyTranspiler]
+	static IEnumerable<CodeInstruction> LimitReadLength(IEnumerable<CodeInstruction> IL)
+	{
+		MethodInfo readBitArray = typeof(NetworkExtensions).GetMethod(
+				nameof(NetworkExtensions.ReadBitArray),
+				new Type[] { typeof(NetworkReader), typeof(bool[]) }
+			);
+
+		foreach ( CodeInstruction instruction in IL )
+		{
+			if ( instruction.Calls(readBitArray) )
+			{
+				yield return Transpilers.EmitDelegate(
+					( NetworkReader input, bool[] array ) =>
 					{
-						instance?.UnpatchSelf();
-						instance = null;
-					}
-				};
-		}
-
-		static Sprite Load(byte[] imageData)
-		{
-			Texture2D texture = new(512, 512, TextureFormat.ARGB32, 4, linear: false);
-			ImageConversion.LoadImage(texture, imageData);
-
-			return Sprite.Create(
-					texture, new Rect(0, 0, texture.width, texture.height),
-					pivot: new Vector2(texture.width / 2, texture.height / 2)
-				);
-		}
-
-		[HarmonyPatch(typeof(RuleCatalog), nameof(RuleCatalog.Init))]
-		[HarmonyFinalizer]
-		static void AddArtifact(Exception __exception)
-		{
-			if ( __exception is not null ) throw __exception;
-			RuleDef rule = RuleDef.FromArtifact(definition.artifactIndex);
-
-			ruleIndex = RuleCatalog.allRuleDefs.Count;
-			rule.globalIndex = ruleIndex;
-
-			RuleCatalog.allRuleDefs.Add(rule);
-			RuleCatalog.ruleDefsByGlobalName[rule.globalName] = rule;
-
-			rule.category = RuleCatalog.artifactRuleCategory;
-			RuleCatalog.artifactRuleCategory.children.Add(rule);
-
-			for ( int i = 0; i < rule.choices.Count; ++i )
-			{
-				RuleChoiceDef choice = rule.choices[i];
-
-				choice.localIndex = i;
-				choice.globalIndex = RuleCatalog.allChoicesDefs.Count;
-
-				RuleCatalog.allChoicesDefs.Add(choice);
-				RuleCatalog.ruleChoiceDefsByGlobalName[choice.globalName] = choice;
+						if ( definition is null ) input.ReadBitArray(array);
+						else input.ReadBitArray(array, array.Length - 1);
+					});
 			}
-		}
-
-		[HarmonyPatch(typeof(RuleCatalog), nameof(RuleCatalog.AddRule))]
-		[HarmonyPrefix]
-		static bool SkipRule(RuleDef ruleDef)
-		{
-			foreach ( RuleChoiceDef choice in ruleDef.choices )
-				if ( choice.artifactIndex == definition.artifactIndex )
-					return false;
-
-			return true;
-		}
-
-		[HarmonyPatch(typeof(EclipseRun), nameof(EclipseRun.OverrideRuleChoices))]
-		[HarmonyPostfix]
-		static void ShowInEclipse(
-				RuleChoiceMask mustInclude, RuleChoiceMask mustExclude)
-		{
-			IEnumerable<RuleChoiceDef> choices = ruleIndex >= 0 ?
-					RuleCatalog.GetRuleDef(ruleIndex).choices : new List<RuleChoiceDef>();
-
-			foreach ( RuleChoiceDef choice in choices )
-			{
-				mustInclude[choice.globalIndex] = false;
-				mustExclude[choice.globalIndex] = false;
-			}
-		}
-
-		[HarmonyPatch(typeof(NetworkExtensions), nameof(NetworkExtensions.Write),
-				new Type[] { typeof(NetworkWriter), typeof(RuleBook) })]
-		[HarmonyPrefix]
-		static bool WriteRuleBook(NetworkWriter writer, RuleBook src)
-		{
-			for ( int i = 0; i < src.ruleValues.Length; ++i )
-				if ( i != ruleIndex )
-					writer.Write(src.ruleValues[i]);
-
-			return false;
-		}
-
-		[HarmonyPatch(typeof(NetworkExtensions), nameof(NetworkExtensions.ReadRuleBook))]
-		[HarmonyPrefix]
-		static bool ReadRuleBook(NetworkReader reader, RuleBook dest)
-		{
-			for ( int i = 0; i < dest.ruleValues.Length; ++i )
-			{
-				if ( i == ruleIndex )
-				{
-					dest.ruleValues[i] =
-							(byte) RuleCatalog.GetRuleDef(ruleIndex).defaultChoiceIndex;
-				}
-				else dest.ruleValues[i] = reader.ReadByte();
-			}
-
-			return false;
-		}
-
-		[HarmonyPatch(typeof(RunArtifactManager), nameof(RunArtifactManager.OnDeserialize))]
-		[HarmonyTranspiler]
-		static IEnumerable<CodeInstruction> LimitReadLength(IEnumerable<CodeInstruction> IL)
-		{
-			MethodInfo readBitArray = typeof(NetworkExtensions).GetMethod(
-					nameof(NetworkExtensions.ReadBitArray),
-					new Type[] { typeof(NetworkReader), typeof(bool[]) }
-				);
-
-			foreach ( CodeInstruction instruction in IL )
-			{
-				if ( instruction.Calls(readBitArray) )
-				{
-					yield return Transpilers.EmitDelegate(
-						( NetworkReader input, bool[] array ) =>
-						{
-							if ( definition is null ) input.ReadBitArray(array);
-							else input.ReadBitArray(array, array.Length - 1);
-						});
-				}
-				else yield return instruction;
-			}
+			else yield return instruction;
 		}
 	}
 }
--- Plugin.cs
+++ Plugin.cs
@@ -10,193 +10,189 @@ using UnityEngine;
 using UnityEngine.Networking;
 using Console = System.Console;
 
-[assembly: AssemblyVersion(Local.Eclipse.CurseCatcher.Plugin.versionNumber)]
+[assembly: AssemblyVersion(Local.Eclipse.CurseCatcher.Plugin.version)]
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
-		// Allow private member access via publicized assemblies.
 
-namespace Local.Eclipse.CurseCatcher
+namespace Local.Eclipse.CurseCatcher;
+
+[BepInPlugin(identifier, "CurseCatcher", version)]
+class Plugin : BaseUnityPlugin
 {
-	[BepInPlugin("local.eclipse.cursecatcher", "CurseCatcher", versionNumber)]
-	public class Plugin : BaseUnityPlugin
+	public const string identifier = "local.eclipse.cursecatcher", version = "0.3.0";
+
+	static bool artifact;
+	static ConfigEntry<bool> self, friendly, fall, interactable, hazard, log;
+
+	protected void Awake()
 	{
-		public const string versionNumber = "0.3.0";
+		const string general = "General", other = "Other";
 
-		private static bool enableArtifact;
-		private static ConfigEntry<bool>
-				selfDamage, friendlyFire, fallDamage, interactableCost, stageHazard, enableLog;
+		self = Config.Bind(
+				section: general,
+				key: "Self Damage",
+				defaultValue: false,
+				description: "Abilities that cost health to activate will curse the player"
+					+ " if this setting is enabled.");
 
-		public void Awake()
+		friendly = Config.Bind(
+				section: general,
+				key: "Friendly Fire",
+				defaultValue: true,
+				description: "Effects that damage allies may apply curse when enabled.");
+
+		fall = Config.Bind(
+				section: general,
+				key: "Fall Damage",
+				defaultValue: true,
+				description: "Inflict curse upon taking sufficient fall damage.");
+
+		hazard = Config.Bind(
+				section: general,
+				key: "Interactable Cost",
+				defaultValue: true,
+				description: "Whether the debuff is triggered upon paying health costs.");
+
+		hazard = Config.Bind(
+				section: general,
+				key: "Neutral Hazards",
+				defaultValue: true,
+				description: "If neutral damage, such as stage hazards, can cause curse.");
+
+		artifact = Config.Bind(
+				section: other,
+				key: "Artifact Enabled",
+				defaultValue: false,
+				description: "Used to toggle the above functionality. Selectable in the"
+					+ " Eclipse lobby, but also applies the modifier to other game modes."
+			).Value;
+
+		log = Config.Bind(
+				section: other,
+				key: "Detailed Logging",
+				defaultValue: false,
+				description: "For troubleshooting purposes only.");
+
+		if ( artifact ) Harmony.CreateAndPatchAll(typeof(Artifact));
+		else Harmony.CreateAndPatchAll(typeof(Plugin));
+	}
+
+	[HarmonyPatch(typeof(HealthComponent), nameof(HealthComponent.TakeDamage))]
+	[HarmonyTranspiler]
+	static IEnumerable<CodeInstruction> InsertCodeInstruction(IEnumerable<CodeInstruction> IL)
+	{
+		MethodInfo getSelectedDifficulty = typeof(Run).GetProperty(
+				nameof(Run.selectedDifficulty)).GetMethod;
+
+		bool found = false;
+		CodeInstruction previousInstruction = null;
+
+		Console.WriteLine("Installing hook for 'Eclipse 8' curse...");
+		foreach ( CodeInstruction instruction in IL )
 		{
-			const string general = "General", other = "Other";
-
-			selfDamage = Config.Bind(
-					section: general,
-					key: "Self Damage",
-					defaultValue: false,
-					description: "Abilities that cost health to activate will curse the player "
-						+ "if this setting is enabled.");
-
-			friendlyFire = Config.Bind(
-					section: general,
-					key: "Friendly Fire",
-					defaultValue: true,
-					description: "Effects that damage allies may apply curse when enabled.");
-
-			fallDamage = Config.Bind(
-					section: general,
-					key: "Fall Damage",
-					defaultValue: true,
-					description: "Inflict curse upon taking sufficient fall damage.");
-
-			interactableCost = Config.Bind(
-					section: general,
-					key: "Interactable Cost",
-					defaultValue: true,
-					description: "Whether the debuff is triggered upon paying health costs.");
-
-			stageHazard = Config.Bind(
-					section: general,
-					key: "Neutral Hazards",
-					defaultValue: true,
-					description: "If neutral damage, such as stage hazards, can cause curse.");
-
-			enableArtifact = Config.Bind(
-					section: other,
-					key: "Artifact Enabled",
-					defaultValue: false,
-					description: "Used to toggle the above functionality. Selectable in the "
-						+ "Eclipse lobby, but also applies the modifier to other game modes."
-				).Value;
-
-			enableLog = Config.Bind(
-					section: other,
-					key: "Detailed Logging",
-					defaultValue: false,
-					description: "For troubleshooting purposes only.");
-
-			if ( enableArtifact ) Harmony.CreateAndPatchAll(typeof(Artifact));
-			else Harmony.CreateAndPatchAll(typeof(Plugin));
-		}
-
-		[HarmonyPatch(typeof(HealthComponent), nameof(HealthComponent.TakeDamage))]
-		[HarmonyTranspiler]
-		private static IEnumerable<CodeInstruction> InsertCodeInstruction(
-				IEnumerable<CodeInstruction> instructionList)
-		{
-			MethodInfo getSelectedDifficulty = typeof(Run).GetProperty(
-					nameof(Run.selectedDifficulty)).GetMethod;
-
-			bool found = false;
-			CodeInstruction previousInstruction = null;
-
-			Console.WriteLine("Installing hook for 'Eclipse 8' curse...");
-			foreach ( CodeInstruction instruction in instructionList )
+			if ( found && instruction.Branches(out _) )
 			{
-				if ( found && instruction.Branches(out _) )
+				found = false;
+
+				if ( previousInstruction.LoadsConstant(DifficultyIndex.Eclipse8) )
 				{
-					found = false;
+					Console.WriteLine("...instruction sequence found.");
 
-					if ( previousInstruction.LoadsConstant(DifficultyIndex.Eclipse8) )
-					{
-						Console.WriteLine("...instruction sequence found.");
+					yield return new CodeInstruction(OpCodes.Pop);
+					yield return new CodeInstruction(OpCodes.Ldarg_1);
 
-						yield return new CodeInstruction(OpCodes.Pop);
-						yield return new CodeInstruction(OpCodes.Ldarg_1);
+					yield return CodeInstruction.Call(
+							typeof(Plugin), nameof(Plugin.ApplyCurse));
 
-						yield return CodeInstruction.Call(
-								typeof(Plugin), nameof(Plugin.ApplyCurse));
-
-						yield return new CodeInstruction(OpCodes.Brfalse, instruction.operand);
-						continue;
-					}
-				}
-				else if ( instruction.Calls(getSelectedDifficulty) )
-					found = true;
-
-				previousInstruction = instruction;
-				yield return instruction;
-			}
-		}
-
-		public static bool ApplyCurse(DifficultyIndex difficulty, DamageInfo damageInfo)
-		{
-			if ( ! enableArtifact && difficulty < DifficultyIndex.Eclipse8 )
-				return false;
-
-			bool curse = true;
-			if ( enableLog.Value ) PrintInfo(damageInfo);
-
-			if ( damageInfo.attacker is null )
-			{
-				if ( damageInfo.damageType.HasFlag(DamageType.FallDamage) )
-					curse = fallDamage.Value;
-				else switch ( damageInfo.damageType )
-				{
-					case DamageType.NonLethal:
-					case DamageType.NonLethal | DamageType.BypassArmor:
-						curse = selfDamage.Value;
-						break;
+					yield return new CodeInstruction(OpCodes.Brfalse, instruction.operand);
+					continue;
 				}
 			}
-			else if ( damageInfo.attacker )
-			{
-				TeamComponent team = damageInfo.attacker.GetComponent<TeamComponent>();
-				if ( team ) switch ( team.teamIndex )
-				{
-					case TeamIndex.Player:
-						curse = friendlyFire.Value;
-						break;
+			else if ( instruction.Calls(getSelectedDifficulty) )
+				found = true;
 
-					case TeamIndex.Neutral:
-						curse = stageHazard.Value;
-						break;
-				}
-				else if ( damageInfo.attacker.GetComponent<IInteractable>() is object )
-					curse = interactableCost.Value;
-			}
-
-			if ( curse ) return true;
-			else
-			{
-				if ( enableLog.Value ) Console.WriteLine("...preventing curse.");
-				return false;
-			}
+			previousInstruction = instruction;
+			yield return instruction;
 		}
+	}
 
-		private static void PrintInfo(DamageInfo damageInfo)
+	public static bool ApplyCurse(DifficultyIndex difficulty, DamageInfo damageInfo)
+	{
+		if ( artifact is false && difficulty < DifficultyIndex.Eclipse8 )
+			return false;
+
+		bool curse = true;
+		if ( log.Value ) PrintInfo(damageInfo);
+
+		if ( damageInfo.attacker is null )
 		{
-			string attacker = getName(damageInfo.attacker),
-					inflictor = getName(damageInfo.inflictor);
-
-			static string getName(GameObject obj)
+			if ( damageInfo.damageType.HasFlag(DamageType.FallDamage) )
+				curse = fall.Value;
+			else switch ( damageInfo.damageType )
 			{
-				string name;
-
-				if ( obj is null ) name = "null";
-				else if ( obj == null ) name = "destroyed";
-				else if ( obj.name is null ) name = "unknown";
-				else name = obj.name;
-
-				int start = name.IndexOf('('), count = name.IndexOf(')') - start + 1;
-				if ( start >= 0 && count >= 0 )
-					name = name.Remove(start, count);
-
-				return name;
+				case DamageType.NonLethal:
+				case DamageType.NonLethal | DamageType.BypassArmor:
+					curse = self.Value;
+					break;
 			}
-
-			string info = "attacker: " + attacker;
-			if ( inflictor != attacker && damageInfo.inflictor is not null )
-				info += " (" + inflictor + ")";
-
-			if ( damageInfo.attacker != null )
-			{
-				TeamComponent team = damageInfo.attacker.GetComponent<TeamComponent>();
-				if ( team != null )
-					info += ", team: " + team.teamIndex;
-			}
-
-			info += ", type: " + damageInfo.damageType;
-			Console.WriteLine(info);
 		}
+		else if ( damageInfo.attacker )
+		{
+			TeamComponent team = damageInfo.attacker.GetComponent<TeamComponent>();
+			if ( team ) switch ( team.teamIndex )
+			{
+				case TeamIndex.Player:
+					curse = friendly.Value;
+					break;
+
+				case TeamIndex.Neutral:
+					curse = hazard.Value;
+					break;
+			}
+			else if ( damageInfo.attacker.GetComponent<IInteractable>() is object )
+				curse = interactable.Value;
+		}
+
+		if ( curse ) return true;
+		else
+		{
+			if ( log.Value ) Console.WriteLine("...preventing curse.");
+			return false;
+		}
+	}
+
+	static void PrintInfo(DamageInfo damage)
+	{
+		string attacker = getName(damage.attacker),
+				inflictor = getName(damage.inflictor);
+
+		static string getName(GameObject obj)
+		{
+			string name;
+
+			if ( obj is null ) name = "null";
+			else if ( obj == null ) name = "destroyed";
+			else if ( obj.name is null ) name = "unknown";
+			else name = obj.name;
+
+			int start = name.IndexOf('('), count = name.IndexOf(')') - start + 1;
+			if ( start >= 0 && count >= 0 )
+				name = name.Remove(start, count);
+
+			return name;
+		}
+
+		string info = "attacker: " + attacker;
+		if ( inflictor != attacker && damage.inflictor is not null )
+			info += " (" + inflictor + ")";
+
+		if ( damage.attacker != null )
+		{
+			TeamComponent team = damage.attacker.GetComponent<TeamComponent>();
+			if ( team != null )
+				info += ", team: " + team.teamIndex;
+		}
+
+		info += ", type: " + damage.damageType;
+		Console.WriteLine(info);
 	}
 }
